345633844b102815f122f8bb4870035cf1b1e39c
Valentine <valentinemaillard1@gmail.com>
2019-11-21 14:58:20 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,51 @@
+require 'pry'
+require_relative 'cat'
+require_relative 'dog'
+
 class Owner
-  # code goes here
+@@all = []
+  attr_reader :name, :species, :new_cat
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+   "I am a #{species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select {|indivi_cat|
+      indivi_cat.owner == self
+    }
+  end
+
+  def dogs
+    Dog.all.select {|indivi_dog|
+      indivi_dog.owner == self
+    }
+  end
+
+  def buy_cat(new_cat)
+    Cat.new(new_cat, self)
+  end 
+
+  def buy_dog(new_dog)
+    Dog.new(new_dog, self)
+  end 
+
 end
